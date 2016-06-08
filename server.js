84e20bf135170b2f887d2c04dee695dbb9f140cc
MICHAEL J MELTON <meltonm83@gmail.com>
2016-06-08 12:57:34 -0700

    Adds Top Tracks To Recommended Artists

--- server.js
+++ server.js
@@ -1,30 +1,75 @@
 var unirest = require('unirest');
 var express = require('express');
 var events = require('events');
+var async = require('async');
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+        .qs(args)
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body);
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
     return emitter;
 };
 
-var relatedArtist = function(artist, res) {
-    var relatedArtistReq = getFromApi('artists/' + artist.id + '/related-artists', {});
-
-    relatedArtistReq.on('end', function(item) {
-        artist.related = item.artists;
-        res.json(artist);
-    })
+var getRelated = function(id) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/artists/' + id + '/related-artists')
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body);
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
+    return emitter;
 };
+
+var getTracks = function(artist) {
+    var emitter = new events.EventEmitter();
+    var topTracks = [];
+
+    for (var i = 0; i < artist.related.length; i++) {
+        var id = artist.related[i].id;
+        topTracks.push({
+            id: id,
+            url: 'https://api.spotify.com/v1/artists/' + id + '/top-tracks?country=GB',
+            tracks: []
+        });
+    }
+
+    addTracks = function(item, callback) {
+        unirest.get(item.url)
+            .end(function(response) {
+                if (response.ok) {
+                    item.tracks = response.body.tracks;
+                    callback(null, item);
+                } else {
+                    callback(response.code);
+                }
+            });
+    };
+
+    async.map(topTracks, addTracks, function(err, res) {
+        if (!err) {
+            res.map(function(item, i) {
+                artist.related[i].tracks = res[i].tracks;
+            });
+            emitter.emit('end', artist);
+        } else {
+            emitter.emit('error', res.code);
+        }
+
+    });
+
+    return emitter;
+};
+
 
 var app = express();
 app.use(express.static('public'));
@@ -38,7 +83,14 @@ app.get('/search/:name', function(req, res) {
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        relatedArtist(artist, res);
+        var artistId = artist.id;
+
+        getRelated(artistId).on('end', function(data) {
+            artist.related = data.artists;
+            getTracks(artist).on('end', function(updated) {
+                res.json(updated);
+            });
+        });
     });
 
     searchReq.on('error', function(code) {
